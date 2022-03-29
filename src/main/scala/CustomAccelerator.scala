b9f5a16ed47c1edb4bbe71dc488b46fcf5b0f1ab
marco66colombo <80001772+marco66colombo@users.noreply.github.com>
2022-03-29 11:52:35 +0200

    config fix

--- src/main/scala/CustomAccelerator.scala
+++ src/main/scala/CustomAccelerator.scala
@@ -1,4 +1,4 @@
-package freechips.rocketchip.tile
+package hppsProject
 
 import chisel3._
 import chisel3.util._
@@ -6,62 +6,7 @@ import chisel3.experimental.IntParam
 import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.rocket._
-import freechips.rocketchip.tilelink._
-
-case object BuildRoCC extends Field[Seq[Parameters => LazyRoCC]](Nil)
-
-class RoCCInstruction extends Bundle {
-  val funct = Bits(7.W)
-  val rs2 = Bits(5.W)
-  val rs1 = Bits(5.W)
-  val xd = Bool()
-  val xs1 = Bool()
-  val xs2 = Bool()
-  val rd = Bits(5.W)
-  val opcode = Bits(7.W)
-}
-
-class RoCCCommand(implicit p: Parameters) extends CoreBundle()(p) {
-  val inst = new RoCCInstruction
-  val rs1 = Bits(xLen.W)
-  val rs2 = Bits(xLen.W)
-  val status = new MStatus
-}
-
-class RoCCResponse(implicit p: Parameters) extends CoreBundle()(p) {
-  val rd = Bits(5.W)
-  val data = Bits(xLen.W)
-}
-
-class RoCCCoreIO(implicit p: Parameters) extends CoreBundle()(p) {
-  val cmd = Flipped(Decoupled(new RoCCCommand))
-  val resp = Decoupled(new RoCCResponse)
-  val mem = new HellaCacheIO
-  val busy = Output(Bool())
-  val interrupt = Output(Bool())
-  val exception = Input(Bool())
-}
-
-class RoCCIO(val nPTWPorts: Int)(implicit p: Parameters) extends RoCCCoreIO()(p) {
-  val ptw = Vec(nPTWPorts, new TLBPTWIO)
-  val fpu_req = Decoupled(new FPInput)
-  val fpu_resp = Flipped(Decoupled(new FPResult))
-}
-
-/** Base classes for Diplomatic TL2 RoCC units **/
-abstract class LazyRoCC(
-      val opcodes: OpcodeSet,
-      val nPTWPorts: Int = 0,
-      val usesFPU: Boolean = false
-    )(implicit p: Parameters) extends LazyModule {
-  val module: LazyRoCCModuleImp
-  val atlNode: TLNode = TLIdentityNode()
-  val tlNode: TLNode = TLIdentityNode()
-}
-
-class LazyRoCCModuleImp(outer: LazyRoCC) extends LazyModuleImp(outer) {
-  val io = IO(new RoCCIO(outer.nPTWPorts))
-}
+import hppsProject._
 
 
 
@@ -94,18 +39,3 @@ class CustomAcceleratorModule(outer: CustomAccelerator)
 }
 
 
-
-class OpcodeSet(val opcodes: Seq[UInt]) {
-  def |(set: OpcodeSet) =
-    new OpcodeSet(this.opcodes ++ set.opcodes)
-
-  def matches(oc: UInt) = opcodes.map(_ === oc).reduce(_ || _)
-}
-
-object OpcodeSet {
-  def custom0 = new OpcodeSet(Seq("b0001011".U))
-  def custom1 = new OpcodeSet(Seq("b0101011".U))
-  def custom2 = new OpcodeSet(Seq("b1011011".U))
-  def custom3 = new OpcodeSet(Seq("b1111011".U))
-  def all = custom0 | custom1 | custom2 | custom3
-}
